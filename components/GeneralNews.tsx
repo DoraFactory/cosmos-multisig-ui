3fc73d79cc874e5672f37015bfd656a2460c77a1
Simon Warta <2603011+webmaster128@users.noreply.github.com>
2025-08-29 18:42:09 +0300

    Update GeneralNews due to DB shutdown

--- components/GeneralNews.tsx
+++ components/GeneralNews.tsx
@@ -9,12 +9,15 @@ export default function GeneralNews({ active }: Props) {
     <Alert className="mx-auto my-4 max-w-md border-yellow-300 bg-fuchsia-900">
       <AlertTitle>Warning!</AlertTitle>
       <AlertDescription className="mt-3">
-        Due to a database migration, all data of multisig.confio.run will be reset
-        on October&nbsp;15<sup>th</sup>,&nbsp;2025.
+        Due to the database shutdown of our database provider, all data of multisig.confio.run will be deleted
+        on September&nbsp;15<sup>th</sup>,&nbsp;2025.
       </AlertDescription>
       <AlertDescription className="mt-3">
-        Please finish your signing processes before or start them after that date. Make sure you
-        have all your public keys ready to re-create the multisig address from public keys + threshold values.
+        Please finish your signing processes before that date. Make sure you
+        have all your public keys ready to re-create the multisig address from public keys + threshold values later on.
+      </AlertDescription>
+      <AlertDescription className="mt-3">
+        At this point we don't know if and when Confio will bring back a hosted version of this tool. 
       </AlertDescription>
     </Alert>
   ) : null;
